97407a2f014e15f5aa2f8303cc6f98779e1d0832
Kwaaak_ <Kwaaak_@1ui-MacBook-Pro.local>
2019-05-15 20:39:01 +0900

    수정본입니다.

--- src/main/java/com/skhu/NWFA/sejongGame/sejongController/sejongController.java
+++ src/main/java/com/skhu/NWFA/sejongGame/sejongController/sejongController.java
@@ -24,28 +24,29 @@ public class sejongController {
 
 	@RequestMapping("KingWordGameLobby")
 	public String KingWordGameLobby(Model model) {
+		
 		return "game/KingWordGameLobby";
 	}
 
 	@RequestMapping("KingWordGame/{sejongId}")
 	public String KingWordGameNext(Model model, @PathVariable String sejongId) {
 		wordsModel words = service.sejongWords(sejongId);
-		int wordlength = words.getJustice().length(); //받아오는 단어 수가 몇 개인지
-		int count = 9-wordlength; //받아오는 단어를 제외한 랜덤된 음절 수를 구하기 위함
+		int wordlength = words.getJustice().length(); // 받아오는 단어 수가 몇 개인지
+		int count = 9 - wordlength; // 받아오는 단어를 제외한 랜덤된 음절 수를 구하기 위함
 		List<syllablesModel> list = service.example(count);
-		List<String> syllablesArray = new ArrayList<String>(); //실제 단어 리스트
-		List<String> wordArray = new ArrayList<String>(Arrays.asList(words.getJustice().split("")));//가짜 음절 리스트
-		for(syllablesModel syllable: list) {
+		List<String> syllablesArray = new ArrayList<String>(); // 실제 단어 리스트
+		List<String> wordArray = new ArrayList<String>(Arrays.asList(words.getJustice().split("")));// 가짜 음절 리스트
+		for (syllablesModel syllable : list) {
 			syllablesArray.add(syllable.getSyllable());
-		}//데이터베이스에서 받아오는 객체는 'syllables'모델 이기 때문에 'string'으로 형 변환
-		syllablesArray.addAll(wordArray); //뷰에 출력하기 위한 9개의 단어를 실제 단어 리스트와 가짜 음절 리스트를 합쳐서 구함
-		Collections.shuffle(syllablesArray); //리스트를 랜덤으로 섞어줌
+		} // 데이터베이스에서 받아오는 객체는 'syllables'모델 이기 때문에 'string'으로 형 변환
+		syllablesArray.addAll(wordArray); // 뷰에 출력하기 위한 9개의 단어를 실제 단어 리스트와 가짜 음절 리스트를 합쳐서 구함
+		Collections.shuffle(syllablesArray); // 리스트를 랜덤으로 섞어줌
 		Random random = new Random();
 		int hintIndex = random.nextInt(wordlength);
 
 		model.addAttribute("sejong", sejongId);
 		model.addAttribute("wordlength", wordlength);
-		model.addAttribute("li",syllablesArray);
+		model.addAttribute("li", syllablesArray);
 		model.addAttribute("word", words);
 		model.addAttribute("hintWord", wordArray.get(hintIndex));
 		model.addAttribute("hintIndex", hintIndex);
